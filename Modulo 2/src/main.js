c85cf1d0b3773eef47f6bf3e6db226e2cd17dd1e
Nombre prueba <prueba@ejemplo.com>
2025-01-17 17:11:00 +0100

    Prácticas 1 y 2

---
+++ Modulo 2/src/main.js
@@ -0,0 +1,4 @@
+import "./style.css";
+
+console.log("Como yo invito a la bebida, del total restamos el importe de las bebidas y dividimos entre 6: "+ (120-18)/6 + "€" + " es lo que debe pagar cada comensal");
+console.log("y como yo invito a la bebida, yo tengo que pagar " + (17+18) + "€")
